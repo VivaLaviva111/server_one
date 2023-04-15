ad759e797d13b607f8defd174e7ed78b1eb940fb
unknown <softwareapps@proton.me>
2023-04-15 13:32:36 -0700

    tc

--- controllers/geoController.js
+++ controllers/geoController.js
@@ -2,14 +2,14 @@ const asyncHandler = require("express-async-handler")
 const Geo = require("../models/geoModel")
 const postGeo  = asyncHandler(async(req,res)=>{
    console.log(req.body);
-   //  const suggestion = await Geo.create(req.body)
-   //  if(suggestion){
-   //    console.log("succes!");
-   //    console.log(suggestion);
-   //    console.log(suggestion);
-   //     res.status(201).json(suggestion)
-   //  } else{
-   //     throw new Error("Failed to create Suggestion try again later")
-   //  }
+    const suggestion = await Geo.create(req.body)
+    if(suggestion){
+      console.log("succes!");
+      console.log(suggestion);
+      console.log(suggestion);
+       res.status(201).json(suggestion)
+    } else{
+       throw new Error("Failed to create Suggestion try again later")
+    }
    })
    module.exports ={postGeo}
