1aa9427a752a2569e18a25ed010e8cb5bd2e07dd
unknown <softwareapps@proton.me>
2023-04-19 10:04:12 -0700

    objGeo

--- controllers/geoController.js
+++ controllers/geoController.js
@@ -2,7 +2,11 @@ const asyncHandler = require("express-async-handler")
 const Geo = require("../models/geoModel")
 const postGeo  = asyncHandler(async(req,res)=>{
    console.log(req.body);
-    const suggestion = await Geo.create(req.body)
+   const body = req.body
+   console.log(body);
+   console.log(JSON.stringify({body}));
+   const objGeo = {geoData:{data:req.body}}
+    const suggestion = await Geo.create(objGeo)
     if(suggestion){
       console.log("succes!");
        res.status(201).json(suggestion)
